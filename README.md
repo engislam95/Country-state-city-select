830b514f3a06627c0b8d1d5e9032c617720d4c2c
unknown <ihassan@tendegrees.sa>
2021-12-02 11:49:12 +0200

    Enhancement/add-props

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Country State City Select
-#### Basic library for Country, State and City with select inputs 
+# Global Country State City Select
+
+#### Basic library for Country, State and City with select inputs
 
 [![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://github.com/engislam95/Country-state-city-select)
 
@@ -7,11 +8,12 @@ Basic library for Country, State and City with select inputs built with vue.js.
 
 - Input select with all countries around the world
 - Select for states of that country
-- Input select with option Cities belong to that state 
+- Input select with option Cities belong to that state
+- Allow to customize it using props
 
 ## Installation
 
-CountryStateCitySelect  requires [Node.js](https://nodejs.org/)  to run.
+CountryStateCitySelect requires [Node.js](https://nodejs.org/) to run.
 
 Install the dependencies and devDependencies and start the server.
 
@@ -20,33 +22,50 @@ cd 'your folder'
 npm i
 npm install -g @vue/cli
 npm install -g @vue/cli-service-global
-npm i global-country-state-city-select 
+npm i global-country-state-city-select
 ```
 
-## Plugins
+## Props
 
-CountryStateCitySelect  is currently extended with the following plugins.
-Instructions on how to use them in your own application are linked below.
+|                    Props                    |  Type   |                 Default                 |                                                         Description                                                          |
+| :-----------------------------------------: | :-----: | :-------------------------------------: | :--------------------------------------------------------------------------------------------------------------------------: | --- |
+|    countryLabel , stateLabel , cityLabel    | String  | Choose your { country , state , city }: |                                            Label to show above the select input.                                             |
+| countryBgColor , stateBgColor , cityBgColor | String  |                 default                 |                 Background color of your input select with white text accept option { blue , red , green }.                  |
+|     countrySize , stateSize , citySize      | String  |            default { 14px }             | Applying {padding , borderRadius , fontSize , lineHeight } of your input select and accept option {small , default , large } |
+| countryRounded , stateRounded , cityRounded | Boolean |            default { True }             |                  Applying { borderRadius of 60px } of your input select and accept option { true , false }                   | 3   |
 
-| Plugin | README |
-| ------ | ------ |
-| Bili | [https://bili.egoist.sh][PlDb] |
-| Rollup.js  | [https://rollupjs.org/guide][PlGh] |
+## Usage Example
 
+```sh
+<global-country-state-city-select :countryLabel="new label form country select" :stateBgColor="blue"  :citySize="large" :countryRounded="false" />
 
-#### How to use it 
+```
+
+## How to import it in your vue app
 
 In your main.js, or a similar entry point for your app:
 
 ```sh
 import SelectCountryStateCity from "SelectCountryStateCity";
 ```
+
 ```sh
 import Vue from "vue";
 ```
+
 ```sh
 Vue.use(SelectCountryStateCity);
 ```
+
+## Plugins
+
+CountryStateCitySelect is currently extended with the following plugins.
+Instructions on how to use them in your own application are linked below.
+
+| Plugin    | README                             |
+| --------- | ---------------------------------- |
+| Bili      | [https://bili.egoist.sh][pldb]     |
+| Rollup.js | [https://rollupjs.org/guide][plgh] |
 
 ## License
 
--- src/SelectCountryStateCity.vue
+++ src/SelectCountryStateCity.vue
@@ -6,9 +6,16 @@
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
       crossorigin="anonymous"
     />
-    <label for="cars">Choose your country :</label>
+    <label for="cars">{{ countryLabel }}</label>
     <select
       class="form-select"
+      :class="[
+        'country-inp-select--' + countryBgColor,
+        'country-inp-select--' + countrySize,
+        {
+          'country-inp-select--rounded': countryRounded,
+        },
+      ]"
       aria-label="Default select country"
       v-model="selectedCountry"
       @change="fireState()"
@@ -22,9 +29,16 @@
         {{ country.name }}
       </option>
     </select>
-    <label for="cars">Choose your state :</label>
+    <label for="cars">{{ stateLabel }}</label>
     <select
       class="form-select"
+      :class="[
+        'state-inp-select--' + stateBgColor,
+        'state-inp-select--' + stateSize,
+        {
+          'state-inp-select--rounded': stateRounded,
+        },
+      ]"
       :disabled="selectedCountry == ''"
       aria-label="Default select country"
       v-model="selectedState"
@@ -35,10 +49,17 @@
         {{ state.name }}
       </option>
     </select>
-    <label for="cars">Choose your city :</label>
+    <label for="cars">{{ cityLabel }}</label>
     <select
-    :disabled="selectedState == ''"
+      :disabled="selectedState == ''"
       class="form-select"
+      :class="[
+        'city-inp-select--' + cityBgColor,
+        'city-inp-select--' + citySize,
+        {
+          'city-inp-select--rounded': cityRounded,
+        },
+      ]"
       aria-label="Default select country"
       v-model="selectedCity"
     >
@@ -56,6 +77,79 @@ import listStates from "./json/state.json";
 import listCities from "./json/city.json";
 
 export default {
+  props: {
+    // country
+    countryLabel: {
+      type: String,
+      default: "Choose your country :",
+    },
+    countryBgColor: {
+      type: String,
+      default: "default",
+      validator(x) {
+        return ["default", "blue", "green", "red"].indexOf(x) !== -1;
+      },
+    },
+    countryRounded: {
+      type: Boolean,
+      default: true,
+    },
+    countrySize: {
+      type: String,
+      default: "default",
+      validator(x) {
+        return ["small", "default", "large"].indexOf(x) !== -1;
+      },
+    },
+
+    // state
+    stateLabel: {
+      type: String,
+      default: "Choose your state :",
+    },
+    stateBgColor: {
+      type: String,
+      default: "default",
+      validator(x) {
+        return ["default", "blue", "green", "red"].indexOf(x) !== -1;
+      },
+    },
+    stateRounded: {
+      type: Boolean,
+      default: true,
+    },
+    stateSize: {
+      type: String,
+      default: "default",
+      validator(x) {
+        return ["small", "default", "large"].indexOf(x) !== -1;
+      },
+    },
+
+    // city
+    cityLabel: {
+      type: String,
+      default: "Choose your city :",
+    },
+    cityBgColor: {
+      type: String,
+      default: "default",
+      validator(x) {
+        return ["default", "blue", "green", "red"].indexOf(x) !== -1;
+      },
+    },
+    cityRounded: {
+      type: Boolean,
+      default: true,
+    },
+    citySize: {
+      type: String,
+      default: "default",
+      validator(x) {
+        return ["small", "default", "large"].indexOf(x) !== -1;
+      },
+    },
+  },
   data() {
     return {
       countries: listCountries,
@@ -97,3 +191,138 @@ export default {
 };
 </script>
 
+<style >
+/* country */
+.country-inp-select--blue {
+  background-color: #0194ef !important;
+  color: #fff !important ;
+}
+
+.country-inp-select--green {
+  background-color: #1bb934 !important;
+  color: #fff !important ;
+}
+
+.country-inp-select--red {
+  background-color: #e1112c !important;
+  color: #fff !important ;
+}
+
+/* state */
+
+.state-inp-select--blue {
+  background-color: #0194ef !important;
+  color: #fff !important ;
+}
+
+.state-inp-select--green {
+  background-color: #1bb934 !important;
+  color: #fff !important ;
+}
+
+.state-inp-select--red {
+  background-color: #e1112c !important;
+  color: #fff !important ;
+}
+
+/* city */
+
+.city-inp-select--blue {
+  background-color: #0194ef !important;
+  color: #fff !important ;
+}
+
+.city-inp-select--green {
+  background-color: #1bb934 !important;
+  color: #fff !important ;
+}
+
+.city-inp-select--red {
+  background-color: #e1112c !important;
+  color: #fff !important ;
+}
+
+/* --> SIZES <-- */
+
+/* country */
+.country-inp-select--small {
+  padding: 8px 10px !important;
+  border-radius: 4px !important;
+  font-size: 12px !important;
+  line-height: 12px !important;
+}
+
+.country-inp-select--default {
+  padding: 12px 14px !important;
+  border-radius: 6px !important;
+  font-size: 14px !important;
+  line-height: 16px !important;
+}
+
+.country-inp-select--large {
+  padding: 16px 18px !important;
+  border-radius: 8px !important;
+  font-size: 16px !important;
+  line-height: 20px !important;
+}
+
+/* state */
+
+.state-inp-select--small {
+  padding: 8px 10px !important;
+  border-radius: 4px !important;
+  font-size: 12px !important;
+  line-height: 12px !important;
+}
+
+.state-inp-select--default {
+  padding: 12px 14px !important;
+  border-radius: 6px !important;
+  font-size: 14px !important;
+  line-height: 16px !important;
+}
+
+.state-inp-select--large {
+  padding: 16px 18px !important;
+  border-radius: 8px !important;
+  font-size: 16px !important;
+  line-height: 20px !important;
+}
+
+/* city */
+
+.city-inp-select--small {
+  padding: 8px 10px !important;
+  border-radius: 4px !important;
+  font-size: 12px !important;
+  line-height: 12px !important;
+}
+
+.city-inp-select--default {
+  padding: 12px 14px !important;
+  border-radius: 6px !important;
+  font-size: 14px !important;
+  line-height: 16px !important;
+}
+
+.city-inp-select--large {
+  padding: 16px 18px !important;
+  border-radius: 8px !important;
+  font-size: 16px !important;
+  line-height: 20px !important;
+}
+/* --> BOOLEANS <-- */
+
+/* country */
+.country-inp-select--rounded {
+  border-radius: 60px !important;
+}
+/* state */
+.state-inp-select--rounded {
+  border-radius: 60px !important;
+}
+/* city */
+.city-inp-select--rounded {
+  border-radius: 60px !important;
+}
+</style>
